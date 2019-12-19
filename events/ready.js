997f8239fe4df397248f7e487e787e220b10c98c
Findus <xd@example.com>
2019-12-19 17:51:11 +0100

    some changes

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,10 @@
 module.exports = async () => {
+    async function a() {
+        var lol = await ef.users.get("442411026684837898").send("Pamiętaj o pojemniku dla Franka!")
+        console.log(lol)
+    }
+    a()
+    setInterval(a, 1800000)
     require('../handlers/plugins')()
     require('../handlers/presence')()
     require('../handlers/update')()
