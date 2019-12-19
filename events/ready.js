5cd32d20dd02022403dbf4fdde1f12ec56c99493
efindus <48939809+efindus@users.noreply.github.com>
2019-12-19 16:48:41 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,4 @@
 module.exports = async () => {
-    function a() {
-        ef.users.get("531375399813513252").send("Pamiętaj o pojemniku dla Franka!") 
-    }
-    setInterval(a, 1800000)
     require('../handlers/plugins')()
     require('../handlers/presence')()
     require('../handlers/update')()
