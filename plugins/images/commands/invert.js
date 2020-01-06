24f18474a74e16e56040f0aeb3bd76d23b8b309e
Findus <xd@example.com>
2020-01-06 21:15:33 +0100

    fix fix

--- plugins/images/commands/invert.js
+++ plugins/images/commands/invert.js
@@ -1,6 +1,6 @@
 exports.output = async ({message, args}) => {
     const user = await ef.utils.users.search(message, args[0])
-    var url = user.avatar == null ? 'https://cdn.discordapp.com/embed/avatars/0.png' : user.displayAvatarURL
+    var url = user.avatar == null ? `https://cdn.discordapp.com/embed/avatars/${user.discriminator % 5}.png` : user.displayAvatarURL
     ef.models.apibadosz({
         object: message,
         title: ':black_joker:',
