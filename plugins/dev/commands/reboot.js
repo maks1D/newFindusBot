1eacd166fd1274f34ffc9068633a284db76bf886
efindus <48939809+efindus@users.noreply.github.com>
2020-02-12 16:53:36 +0100

    Create reboot.js

---
+++ plugins/dev/commands/reboot.js
@@ -0,0 +1,32 @@
+exports.output = async ({message, guild, args}) => {
+
+    async function emoji(emojiname) { return await ef.utils.emoji.get(emojiname, message) }
+
+    ef.models.send({
+
+        object: message,
+
+        message: `${await emoji('markYes')}Pomyślnie rozpoczęto restart bota.`
+
+    })
+
+    process.exit()
+
+}
+
+exports.data = {
+
+    triggers: ['reboot'],
+
+    description: 'Restartuje bota.',
+
+    usage: [
+
+        '{prefix}{command}'
+
+    ],
+
+    developer: true
+
+}
+
