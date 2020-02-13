926880ce65181bf1d644ddef6ccdb19acadc9c2e
efindus <48939809+efindus@users.noreply.github.com>
2020-02-13 17:21:31 +0100

    Update tweet.js

--- plugins/images/commands/tweet.js
+++ plugins/images/commands/tweet.js
@@ -1,4 +1,5 @@
 exports.output = async ({message, guild, args}) => {
+    return
     ef.models.apibadosz({
         object: message,
         endpoint: `tweet?text=${encodeURIComponent(args.join(' '))}&username=${encodeURIComponent(message.author.username)}&url=${encodeURIComponent(message.author.displayAvatarURL)}`
@@ -10,6 +11,7 @@ exports.data = {
     description: 'Generuje tweet.',
     usage: [
         '{prefix}{command} <treść tweeta>'     
-    ]
+    ], 
+    hiddenInHelp: true
 }
   
