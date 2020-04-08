3babcb6cde74bd83d8b0af1fb12aaebe97607de8
Findus <xd@example.com>
2020-04-08 11:42:46 +0200

    fixed hotfix

--- plugins/basic/commands/stats.js
+++ plugins/basic/commands/stats.js
@@ -2,15 +2,15 @@ exports.output = async ({message, guild}) => {
     var translations = {pl: ``, en: ``, ru: ``}
     translations.pl = 
 `Ping api: \`${Math.floor(ef.ping)}ms.\`
-Ping bota: \`${message.createdTimestamp - Date.now()}ms.\`
+Ping bota: \`${Math.abs(Date.now() - message.createdTimestamp)}ms.\`
 Wykonane komendy: \`${ef.db.cache['botinfo'][0].commandsdone}\``
     translations.en = 
 `Api ping: \`${Math.floor(ef.ping)}ms.\`
-Bot ping: \`${message.createdTimestamp - Date.now()}ms.\`
+Bot ping: \`${Math.abs(Date.now() - message.createdTimestamp)}ms.\`
 Commands Used: \`${ef.db.cache['botinfo'][0].commandsdone}\``
     translations.ru =
 `API-пинг: \`${Math.floor(ef.ping)}ms.\`
-бот пинг: \`${message.createdTimestamp - Date.now()}ms.\`
+бот пинг: \`${Math.abs(Date.now() - message.createdTimestamp)}ms.\`
 Используемые команды: \`${ef.db.cache['botinfo'][0].commandsdone}\``
     ef.models.send({
         object: message,
