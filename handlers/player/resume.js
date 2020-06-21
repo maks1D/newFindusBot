8e7cb5a0879557e0c28575761772c1b73e53c8ad
Findus <48939809+efindus@users.noreply.github.com>
2020-06-21 12:43:04 +0200

    cleanup after old player

--- handlers/player/resume.js
+++
@@ -1,8 +0,0 @@
-module.exports = async function pause(message) {
-    if(ef.queue[message.guild.id] && ef.queue[message.guild.id].nowPlaying != ''){
-        ef.queue[message.guild.id].player.player.currentStream.dispatcher.paused = false
-        return true
-    } else {
-        return false
-    }
-}
--- handlers/player/skip.js
+++
@@ -1,8 +0,0 @@
-module.exports = async function skip(message) {
-    if(ef.queue[message.guild.id] && ef.queue[message.guild.id].nowPlaying != ''){
-        ef.queue[message.guild.id].player.end()
-        return true
-    } else {
-        return false
-    }
-}
