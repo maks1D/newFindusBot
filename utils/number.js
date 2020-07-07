29c9dbc8c899e4c84e8e1f29779c6ab8b9e16bad
efindus <48939809+efindus@users.noreply.github.com>
2020-07-07 10:16:54 +0200

    better? hope so!

--- utils/number.js
+++ utils/number.js
@@ -3,7 +3,7 @@ const { createHash } = require("crypto");
 exports.random = async (Min, Max) =>
 {
     var Modulo = Max - Min + 1;
-    var Number = createHash("sha256").update(Date.now().toString()).digest("hex");
+    var Number = createHash("sha256").update(`${Date.now().toString()}${Math.random()}`).digest("hex");
     var Result = 0;
 
     for(var Index = 0; Index < Number.length; Index++)
