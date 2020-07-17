7860516d9438e5b50f543422d3556d9c735d6e83
Spaghetti1337 <40419155+Spaghetti1337@users.noreply.github.com>
2020-07-17 16:46:48 +0200

    jeszcze lepsze rng
    * Update number.js
    
    * Update number.js

--- utils/number.js
+++ utils/number.js
@@ -1,15 +1,6 @@
-const { createHash } = require("crypto");
+const { randomBytes } = require("crypto");
 
 exports.random = async (Min, Max) =>
 {
-    var Modulo = Max - Min + 1;
-    var Number = createHash("sha256").update(`${Date.now().toString()}${Math.random()}`).digest("hex");
-    var Result = 0;
-
-    for(var Index = 0; Index < Number.length; Index++)
-    {
-        Result = (Result * 16 + parseInt(Number[Index], 16)) % Modulo;
-    }
-
-    return Min + Result;
+    return Min + Math.floor(randomBytes(4).readUInt32LE() / 0xffffffff * (Max - Min + 1));
 }
