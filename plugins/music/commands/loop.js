b0a89b3aa028605715e4bc6a1392c8ea84a3ee65
efindus <48939809+efindus@users.noreply.github.com>
2020-10-04 12:52:36 +0200

    Ehhh... This awful bugs...

--- plugins/music/commands/loop.js
+++ plugins/music/commands/loop.js
@@ -7,7 +7,7 @@ exports.output = async ({message, guild, args}) => {
         translations.pl[0] = `${ef.emotes.markNo} Nic nie jest aktualnie odtwarzane.`
         translations.en[0] = `${ef.emotes.markNo} Nothing is currently playing.`
         translations.ru[0] = `${ef.emotes.markNo} Ничего в данный момент не играет.`
-        ef.models.send({
+        return ef.models.send({
             object: message,
             message: `${translations[guild.settings.language][0]}`,
             color: ef.colors.red
