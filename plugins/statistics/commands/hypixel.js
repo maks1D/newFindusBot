45bf95acfe14fe95ae6e87518da8ee92653768ca
Findus <48939809+efindus@users.noreply.github.com>
2021-01-05 22:54:34 +0100

    Fixed hypixelstats command

--- plugins/statistics/commands/hypixel.js
+++ plugins/statistics/commands/hypixel.js
@@ -1,12 +1,18 @@
+const { Attachment } = require('discord.js')
+
 const baseURL = "https://gen.plancke.io/exp/";
-const fs = require("fs");
 var translations = {en: [], pl: [], ru: []}
 
-async function send(user, message, guild)  {
-    const response = await ef.http.get(`${baseURL}${user}.png`)
-    fs.writeFileSync(`${__dirname}/${user}.png`, response.body)
-    var data = fs.readFileSync(`${__dirname}/${user}.png`, 'utf8')
-    if(data == "ensure==BadPlayerException"){
+exports.output = async ({message, guild, args}) => {
+    var user = args.join(' ');
+    translations.pl[0] = `${ef.emotes.markNo}Podaj poprawną nazwę użytkownika!`
+    translations.en[0] = `${ef.emotes.markNo}Please enter a valid username!`
+    translations.ru[0] = `${ef.emotes.markNo}Пожалуйста введите действительное имя пользователя!`
+    if(/[^a-z0-9_]/i.test(user)) return ef.models.send({object: message, message: `${translations[guild.settings.language][0]}`, color: ef.colors.red})
+    
+    const response = await ef.http.get(`${baseURL}${user}.png`);
+
+    if(Buffer.from(response.body).toString('utf8') == "ensure==BadPlayerException"){
         translations.pl[0] = `${ef.emotes.markNo}Użytkownik nie istnieje!`
         translations.en[0] = `${ef.emotes.markNo}User does not exist!`
         translations.ru[0] = `${ef.emotes.markNo}Пользователь не существует!`
@@ -16,26 +22,20 @@ async function send(user, message, guild)  {
             color: ef.colors.red
         })
     } else {
+        const file = new Attachment(response.body, `${user}.png`)
+        
         translations.pl[0] = `Statystyki dla ${user}`
         translations.en[0] = `Stats for ${user}`
         translations.ru[0] = `Статистика для ${user}`
+        
         await ef.models.send({
             object: message,
             message: ``,
+            file: file,
             image: `attachment://${user}.png`,
             footer: `${translations[guild.settings.language][0]}`
         })
     }
-    fs.unlinkSync(`${__dirname}/${user}.png`, (err) => {if(err != null) throw err})
-}
-
-exports.output = async ({message, guild, args}) => {
-    var user = args.join(' ');
-    translations.pl[0] = `${ef.emotes.markNo}Podaj poprawną nazwę użytkownika!`
-    translations.en[0] = `${ef.emotes.markNo}Please enter a valid username!`
-    translations.ru[0] = `${ef.emotes.markNo}Пожалуйста введите действительное имя пользователя!`
-    if(/[^a-z0-9_]/i.test(user)) return ef.models.send({object: message, message: `${translations[guild.settings.language][0]}`, color: ef.colors.red})
-    send(user, message, guild);
 }
 
 exports.data = {
